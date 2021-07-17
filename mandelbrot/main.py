e995d88c9d6a0febe3b1cbf7889b19661c740a63
tomabou <kazu.8128.9.6@gmail.com>
2021-07-17 19:41:12 +0900

    fix mande

--- mandelbrot/main.py
+++ mandelbrot/main.py
@@ -1,25 +1,23 @@
 from PIL import Image
-import sys
-import numpy as np
-import random
+import math
 
 
 def make_image():
     color1 = (0x66, 0x99, 0x99)  # 0の色
-    color2 = (0xff, 0x99, 0x99)  # 1の色
-    x = 1920
-    y = 1080  # 出力画像サイズ　今はフルHDの画像を出力します
-    center = -1 + 0.291j
-    cell = 0.00003
-    dotsize = 1  # 一つのセルがドットいくつ分か
+#    color2 = (0xff, 0x99, 0x99)  # 1の色
+    x_size = 1800
+    y_size = 2700  # 出力画像サイズ　今はフルHDの画像を出力します
+    center = -1 + 0.3j
+    cell = 0.00004
+    dotsize = 2  # 一つのセルがドットいくつ分か
     filename = str(center) + " " + str(cell) + "a.png"
-    img = Image.new('RGB', (x, y), color1)
-    nx = x//dotsize + 1
-    ny = y//dotsize + 1
+    img = Image.new('RGB', (x_size, y_size), color1)
+    nx = x_size//dotsize + 1
+    ny = y_size//dotsize + 1
     cell_map = [[0 for i in range(ny)] for j in range(nx+2)]
 
-    xcenter = x/2
-    ycenter = y/2
+    xcenter = nx/2
+    ycenter = ny/2
 
     for y in range(ny):
         for x in range(nx):
@@ -29,14 +27,19 @@ def make_image():
             for n in range(400):
                 count = count+1
                 z = z*z + c
-                if z.real > 3.0 or z.imag > 3.0 or z.real < -3.0 or z.imag < -3.0:
+                if abs(z.real) > 3.0 or abs(z.imag) > 3.0:
                     break
             cell_map[x][y] = count
 
-    for i in range(x):
-        for j in range(y):
+    for i in range(x_size):
+        for j in range(y_size):
             num = cell_map[i//dotsize][j//dotsize]
-            num = abs(num-128)*2
+            # num = abs(num-128)*2
+            # num = num * num // 100
+            # num = - math.log((num+1)) / math.log(400) * 256
+            num = (num - 20) * 5
+            num = min(max(0, int(num)), 255)
+            # num = 0 if num < 50 else 255
 #            if num<100:
 #                color = (0xff - num * 256 //100, 0xff,0xff)
 #            elif num<200:
