8ec19f6d0e0357ae4aac5b2305e85daeb3dcf975
tomabou <kazu.8128.9.6@gmail.com>
2021-07-19 18:07:48 +0900

    add numba

--- atractor/main.py
+++ atractor/main.py
@@ -1,18 +1,19 @@
 from PIL import Image, ImageDraw
 import sys
-import random
-import numpy as np
 import math
 from typing import Tuple, List
+from numba import jit
 
 k = 0.1
 B = 12.0
 
 
+@jit
 def f1(x: float, y: float, t: float) -> Tuple[float, float]:
     return y, -k*y - x * x * x + B * math.cos(t)
 
 
+@jit
 def runge_kutta(x: float, y: float,
                 t: float, dt: float) -> Tuple[float, float, float]:
     dx1, dy1 = f1(x, y, t)
@@ -24,13 +25,17 @@ def runge_kutta(x: float, y: float,
     return nx, ny, t + dt
 
 
+@jit
 def run_scheme(T, dt) -> List[Tuple[float, float]]:
     t = 0.0
-    x = 0
-    y = 3
+    x = 0.0
+    y = 3.0
     ret = []
-    while True:
-        ret.append((x, y))
+    n = int(T/dt)
+    for _ in range(n):
+        if t > 2 * math.pi:
+            ret.append((x, y))
+            t -= 2 * math.pi
         x, y, t = runge_kutta(x, y, t, dt)
         if t > T:
             break
@@ -38,24 +43,25 @@ def run_scheme(T, dt) -> List[Tuple[float, float]]:
 
 
 def make_image(rule, mode="simple", p=0.5):
-    color1 = (0, 0, 0)  # 0の色
+    color1 = (20, 20, 20)  # 0の色
     color2 = (255, 255, 255)  # 1の色
     x_size = 2518
     y_size = 2991
-    x_center = 0.0
+    x_center = 3.0
     y_center = 0.0
-    x_range = 20.0
+    x_range = 2.0
+    y_range = 17.0
     circle_size: int = 5
-    dt = 0.001
+    dt = 0.01
     filename = "rule"+str(rule)+"_"+str(x_size)+"x"+str(y_size) + \
         "_"+mode+".png"
     img: Image = Image.new('RGB', (x_size, y_size), color1)
     draw = ImageDraw.Draw(img)
 
-    data = run_scheme(1000, dt)
+    data = run_scheme(100000, dt)
     for (a, b) in data:
         circle_center_x = (a-x_center) * x_size / x_range
-        circle_center_y = (b-y_center) * x_size / x_range
+        circle_center_y = (b-y_center) * y_size / y_range
         circle_center_x = int(circle_center_x) + x_size//2
         circle_center_y = int(circle_center_y) + y_size//2
         xy = (
